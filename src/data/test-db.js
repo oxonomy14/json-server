163c0d14deebd01df03be6cd96e608891a4f7010
Andrii Semenenko <oxonomy14@gmail.com>
2025-02-15 21:59:06 +0700

    start2

---
+++ src/data/test-db.js
@@ -0,0 +1,88 @@
+{
+  "users": [
+    {
+      "id": 1,
+      "name": "Ольга",
+      "email": "olga@example.com",
+      "role": "customer"
+    },
+    {
+      "id": 2,
+      "name": "Анна",
+      "email": "anna@example.com",
+      "role": "admin"
+    },
+    {
+      "id": 3,
+      "name": "Марія",
+      "email": "maria@example.com",
+      "role": "customer"
+    },
+    {
+      "id": 4,
+      "name": "Ірина",
+      "email": "irina@example.com",
+      "role": "seller"
+    }
+  ],
+  "products": [
+    {
+      "id": 101,
+      "name": "Футболка с принтом 'Вдохновение'",
+      "price": 599,
+      "size": "M",
+      "material": "100% хлопок"
+    },
+    {
+      "id": 102,
+      "name": "Футболка 'Сила стиля'",
+      "price": 649,
+      "size": "L",
+      "material": "100% хлопок"
+    },
+    {
+      "id": 103,
+      "name": "Футболка 'Энергия жизни'",
+      "price": 699,
+      "size": "S",
+      "material": "100% хлопок"
+    },
+    {
+      "id": 104,
+      "name": "Футболка 'Натхнення'",
+      "price": 720,
+      "size": "XL",
+      "material": "100% хлопок"
+    }
+  ],
+  "orders": [
+    {
+      "order_id": 5001,
+      "user_id": 1,
+      "product_id": 101,
+      "quantity": 2,
+      "status": "pending"
+    },
+    {
+      "order_id": 5002,
+      "user_id": 3,
+      "product_id": 103,
+      "quantity": 1,
+      "status": "shipped"
+    },
+    {
+      "order_id": 5003,
+      "user_id": 2,
+      "product_id": 104,
+      "quantity": 3,
+      "status": "delivered"
+    },
+    {
+      "order_id": 5004,
+      "user_id": 4,
+      "product_id": 102,
+      "quantity": 1,
+      "status": "canceled"
+    }
+  ]
+}
